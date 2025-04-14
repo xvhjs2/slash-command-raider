89f208b9ed7ea5cc39b7cb750520da9af135a061
xvhjs2 <tramtram4400@gmail.com>
2025-04-14 15:53:32 -0400

    Add files via upload

---
+++ slashcmdradier.py
@@ -0,0 +1,33 @@
+import os
+try:
+    import discord
+    from discord.ext import commands
+    import asyncio
+except:
+    os.system('pip install discord.py asyncio')
+    os.system('cls')
+    print('restart the script it should work now')
+    os.system('pause')
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.guilds = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    await bot.tree.sync()
+    print(f"Logged in as {bot.user} (ID: {bot.user.id})")
+
+@bot.tree.command(name="sp", description="spam message")
+async def raid(interaction: discord.Interaction):
+    await interaction.response.send_message("fuck mibu", ephemeral=True)
+
+    await asyncio.sleep(1)
+
+    for i in range(5):
+        await interaction.followup.send("@everyone")
+        await asyncio.sleep(0.5)
+
+bot.run("enter your bot token here")
